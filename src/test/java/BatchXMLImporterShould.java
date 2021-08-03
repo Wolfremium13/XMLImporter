c76fc2c56648d585d5ce5363f433a193a81d0942
kevin.hierro <kevin.hierro@guidesmiths.com>
2021-08-03 16:54:51 +0100

    🔧 Rename BatchXmlImporterShould

--- src/test/java/BatchXMLImporterShould.java
+++ src/test/java/BatchXMLImporterShould.java
@@ -3,16 +3,17 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BatchXmlImporterShould {
+class BatchXmlImporterShould {
 
     final String path = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources";
 
     @Test
-    public void import_xml_into_database() throws JAXBException, IOException {
-        PostgresDao dao = new PostgresDao();
+    public void import_xml_into_database() throws JAXBException, IOException, SQLException {
+        Dao dao = new Dao();
         FileFinder finder = new FileFinder();
         BatchXmlImporter batchXmlImporter = new BatchXmlImporter(dao, finder);
         dao.clearTables();
